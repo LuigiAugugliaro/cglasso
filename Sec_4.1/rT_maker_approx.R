3fa496e99b675fb6373934ae3d27c794679eb94f
LuigiAugugliaro <40381353+LuigiAugugliaro@users.noreply.github.com>
2018-06-26 12:31:58 +0200

    Add files via upload

--- Sec_4.1/rT_maker_approx.R
+++ Sec_4.1/rT_maker_approx.R
@@ -1,11 +1,10 @@
 rT_maker_approx <- function(T1_obs, T2_obs, k, X, n_mis, row_mis, col_mis, muh, Thetah){
-    # inizializzazione statistiche T1 e T2
     T1_bar <- T1_obs
     T2_bar <- T2_obs
     for(h in 1:n_mis){
-        i <- row_mis[h]     # indice della riga con almeno un missing
-        ri <- col_mis[[h]]  # indice delle colonne con missing nell'i-esima riga
-        x_obs <- X[i, -ri]  # sub-vettore osservato nell'i-esima riga
+        i <- row_mis[h]
+        ri <- col_mis[[h]]
+        x_obs <- X[i, -ri]
         muh_obs <- muh[-ri]
         muh_mis <- muh[ri]
         Thetah_obs <- Thetah[-ri, -ri, drop = FALSE]
@@ -21,7 +20,6 @@ rT_maker_approx <- function(T1_obs, T2_obs, k, X, n_mis, row_mis, col_mis, muh, 
             tmean[m] <- out$tmean
             tvar[m, m] <- out$tvar
         }
-        #update delle statistiche di sintesi
         T1_bar[ri] <- T1_bar[ri] + tmean
         T2_bar[-ri, ri] <- T2_bar[-ri, ri, drop = FALSE] + outer(x_obs, tmean)
         T2_bar[ri, -ri] <- t(T2_bar[-ri, ri, drop = FALSE])
--- Sec_4.1/rT_maker_exact.R
+++ Sec_4.1/rT_maker_exact.R
@@ -1,11 +1,10 @@
 rT_maker_exact <- function(T1_obs, T2_obs, k, X, n_mis, row_mis, col_mis, muh, Thetah){
-    # inizializzazione statistiche T1 e T2
     T1_bar <- T1_obs
     T2_bar <- T2_obs
     for(h in 1:n_mis){
-        i <- row_mis[h]     # indice della riga con almeno un missing
-        ri <- col_mis[[h]]  # indice delle colonne con missing nell'i-esima riga
-        x_obs <- X[i, -ri]  # sub-vettore osservato nell'i-esima riga
+        i <- row_mis[h]
+        ri <- col_mis[[h]]
+        x_obs <- X[i, -ri]
         muh_obs <- muh[-ri]
         muh_mis <- muh[ri]
         Thetah_obs <- Thetah[-ri, -ri, drop = FALSE]
@@ -15,7 +14,6 @@ rT_maker_exact <- function(T1_obs, T2_obs, k, X, n_mis, row_mis, col_mis, muh, T
         Sigmah_mis_given_obs <- drop(solve(Thetah_mis))
         muh_mis_given_obs <- muh_mis - drop(Sigmah_mis_given_obs %*% zeta)
         out <- mtmvnorm(mean = muh_mis_given_obs, sigma = Sigmah_mis_given_obs, lower = k[ri])
-        #update delle statistiche di sintesi
         T1_bar[ri] <- T1_bar[ri] + out$tmean
         T2_bar[-ri, ri] <- T2_bar[-ri, ri, drop = FALSE] + outer(x_obs, out$tmean)
         T2_bar[ri, -ri] <- t(T2_bar[-ri, ri, drop = FALSE])
